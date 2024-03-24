c9cdc5cfd7112a2cb217f82d39bcb29aa0c4a125
69icebaby <a_shattykuly@kbtu.kz>
2024-03-24 23:41:58 +0500

    commit7

---
+++ zashita.py
@@ -0,0 +1,14 @@
+'''with open('file.txt','w') as file:
+    input_file = input()
+    file.write(input_file) 
+
+with open('file.txt', 'r') as file:
+    n = file.read()
+    print(n)'''
+'''import re
+t = input()
+x = re.findall(r"[A-Z]", t)
+print(x)'''
+from datetime import datetime 
+n = datetime.now()
+print(n)
